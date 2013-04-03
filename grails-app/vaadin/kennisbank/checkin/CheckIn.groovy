758be31d0534afc3983f38b405168defa86a65a2
Marcelo Dias Avelino <valorcurse@gmail.com>
2013-04-03 16:55:15 +0200

    Checkout is working. Redesigning main checkin page.

--- grails-app/vaadin/kennisbank/checkin/CheckIn.groovy
+++ grails-app/vaadin/kennisbank/checkin/CheckIn.groovy
@@ -29,68 +29,71 @@ class CheckIn extends UI {
 		view.setSizeFull()
 		view.setPrimaryStyleName("check-in")
 
-		Panel checkinChoicePanel = new Panel()
-		checkinChoicePanel.setPrimaryStyleName("island-panel")
-		view.addComponent(checkinChoicePanel)
-		checkinChoicePanel.setWidth("700px")
-		view.setComponentAlignment(checkinChoicePanel, Alignment.MIDDLE_CENTER)
+		Panel checksChoicePanel = new Panel()
+		checksChoicePanel.setPrimaryStyleName("island-panel")
+		view.addComponent(checksChoicePanel)
+		checksChoicePanel.setWidth("700px")
+		view.setComponentAlignment(checksChoicePanel, Alignment.MIDDLE_CENTER)
 
-		VerticalLayout checkInLayout = new VerticalLayout()
-		checkinChoicePanel.setContent(checkInLayout)
-		checkInLayout.setSizeFull()
-		checkInLayout.setSpacing(true)
-		checkInLayout.setMargin(true)
-
+		VerticalLayout mainLayout = new VerticalLayout()
+		checksChoicePanel.setContent(mainLayout)
+		mainLayout.setSizeFull()
 
 		// ------------------------------------------------------- Greeting -------------------------------------------------------
 
 		Label greetingMessageLabel = new Label()
-		checkInLayout.addComponent(greetingMessageLabel)
-		checkInLayout.setComponentAlignment(greetingMessageLabel, Alignment.TOP_CENTER)
+		mainLayout.addComponent(greetingMessageLabel)
+		mainLayout.setComponentAlignment(greetingMessageLabel, Alignment.TOP_CENTER)
 		greetingMessageLabel.setSizeUndefined()
 		greetingMessageLabel.setValue("Ben je een student of een bedrijf?")
 
+		HorizontalLayout checkInOutLayout = new HorizontalLayout()
+		mainLayout.addComponent(checkInOutLayout)
 
-		// ------------------------------------------------------- Buttons -------------------------------------------------------
+		VerticalLayout checkInLayout = new VerticalLayout()
+		checkInOutLayout.addComponent(checkInLayout)
+		checkInLayout.setSizeFull()
+		checkInLayout.setSpacing(true)
+		checkInLayout.setMargin(true)
 
-		HorizontalLayout buttonsLayout = new HorizontalLayout()
-		checkInLayout.addComponent(buttonsLayout)
-		checkInLayout.setComponentAlignment(buttonsLayout, Alignment.MIDDLE_CENTER)
-		buttonsLayout.setSpacing(true)
+		// ------------------------------------------------------- Check in -------------------------------------------------------
 
+		Label checkInLabel = new Label("Hier kun je in checken", ContentMode.HTML)
+		checkInLayout.addComponent(checkInLabel)
 
 		// ------------------------------------------------------- Student Button -------------------------------------------------------
 
 		Button studentButton = new Button("Check In");
-		buttonsLayout.addComponent(studentButton);
-		buttonsLayout.setComponentAlignment(studentButton, Alignment.MIDDLE_CENTER)
+		checkInLayout.addComponent(studentButton);
+		checkInLayout.setComponentAlignment(studentButton, Alignment.MIDDLE_CENTER)
 		//studentButton.setStyleName(Reindeer.BUTTON_LINK);
 		//studentButton.addStyleName("image-button");
 		//studentButton.setIcon(new ThemeResource("student.jpg"));
 		studentButton.addClickListener(new ClickListener() {
-					@Override
-					public void buttonClick(ClickEvent secondEvent) {
-						CheckinWindow window = new CheckinWindow()
-						UI.getCurrent().addWindow(window)
-					}
-				});
+			@Override
+			public void buttonClick(ClickEvent secondEvent) {
+				CheckinWindow window = new CheckinWindow()
+				UI.getCurrent().addWindow(window)
+				window.focus()
+			}
+			});
 
 
 		// ------------------------------------------------------- Company Button -------------------------------------------------------
 
 		Button companyButton = new Button("Check Out");
-		buttonsLayout.addComponent(companyButton);
-		buttonsLayout.setComponentAlignment(companyButton, Alignment.MIDDLE_CENTER)
+		checkInLayout.addComponent(companyButton);
+		checkInLayout.setComponentAlignment(companyButton, Alignment.MIDDLE_CENTER)
 		// companyButton.setStyleName(Reindeer.BUTTON_LINK);
 		// companyButton.addStyleName("image-button");
 		// companyButton.setIcon(new ThemeResource("company.jpg"))
 		companyButton.addClickListener(new ClickListener() {
-					@Override
-					public void buttonClick(ClickEvent event) {
-						CheckoutWindow window = new CheckoutWindow()
-						UI.getCurrent().addWindow(window)
-					}
-				});
+			@Override
+			public void buttonClick(ClickEvent event) {
+				CheckoutWindow window = new CheckoutWindow()
+				UI.getCurrent().addWindow(window)
+			}
+			});
 
 
 		setContent(viewPanel)
